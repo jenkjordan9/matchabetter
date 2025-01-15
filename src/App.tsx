8c5578e80a7a73a5b19301f2b833cca30ebc9496
jenkjordan9 <jenk.ali9@outlook.com>
2025-01-15 16:03:35 +0000

    setup for repo

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import matchalogo from './assets/WhatsApp Image 2025-01-15 at 15.58.08_bec40c1f.jpg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <img src={matchalogo} className="full-screen-image"/>
+    </>
+  )
+}
+
+export default App
